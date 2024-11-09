85b87c69d95b6b183cedd40496fba4f1b5905ee1
Rahul Rajput <rahul242rajput@gmail.com>
2024-11-09 13:41:15 -0500

    second modification to make code readable

--- 10MoreOnCompositeApis/src/views/HomeView.vue
+++ 10MoreOnCompositeApis/src/views/HomeView.vue
@@ -1,43 +1,24 @@
 <script setup>
-// TODO: Functinality for if the tag is pressed give four space
-
 import { onMounted, ref } from 'vue';
-// TODO: This is how we can access eny element from the componenet
-// Just pass this varibale name inside that component
-let textarea = ref(null);  // helps to just get the reference of that elemenet
-// console.log(textarea);
+let textarea = ref(null);  
 
-
-// The mounted is helpful to get the html of the given ref
-onMounted(() => {
-  //To get the value inside the give elemenet, we must give value.value because we want value inside the text area
-  // console.log(textarea.value.value);
-
-// We must give textarea.value to access the elemenet
-textarea.value.addEventListener('keydown', (e) => {
+function onKeydown(e){
   let t = textarea.value;
-  // This code is of tab key
   if(e.key === 'Tab') {
-  
-    // this is how we do it get the first and end 
     let val = t.value,
     start = t.selectionStart,
     end = t.selectionEnd
-    
-    // set t value to: text before caret + tag + text after caret
-    t.value = val.substring(0, start) + '\t' + val.substring(end);
 
-    // put caret at right position again
+    t.value = val.substring(0, start) + '\t' + val.substring(end);
     t.selectionStart = t.selectionEnd = start + 1;
 
     e.preventDefault(); 
   }
-})
-})
+}
 </script>
 
 <template>
   <main>
-    <textarea ref="textarea" style="width: 100%; height: 300px;">Hi There!</textarea>
+    <textarea ref="textarea" @keydown="onKeydown" style="width: 100%; height: 300px;">Hi There!</textarea>
   </main>
 </template>
