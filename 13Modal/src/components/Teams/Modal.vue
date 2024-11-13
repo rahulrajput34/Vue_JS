33233b7dcb4b2396f0dfb49511bf03936f1afc4e
Rahul Rajput <rahul242rajput@gmail.com>
2024-11-13 18:30:31 -0500

    Modal

---
+++ 13Modal/src/components/Teams/Modal.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+    show: Boolean
+})
+</script>
+
+<template>
+    <div v-if="show" class="modal-mask">
+        <div class="modal-container">
+            <header>
+                <slot name="header">Default header</slot>
+            </header>
+            <div>
+                <slot name="body">Default body</slot>
+            </div>
+            <footer  >
+                <slot name="footer">
+                    <button 
+                        @click="$emit('close')"
+                    >
+                        Default close
+                    </button>
+                </slot>
+            </footer>
+        </div>
+    </div>
+
+</template>
+
+<style>
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+    display: grid;
+    place-items: center;
+}
+.modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ 13Modal/src/views/TeamView.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+import TeamHeader from '@/components/Teams/TeamHeader.vue';
+import TeamMembers from '@/components/Teams/TeamMembers.vue';
+import teamFooter from '@/components/Teams/teamFooter.vue';
+import { useTeamStore } from '@/stores/teamStore.js';
+import Modal from '@/components/Teams/Modal.vue';
+
+let team = useTeamStore();
+team.fill();
+
+let showModal = ref(false);
+
+setTimeout(() => {
+  team.spots = 10;
+}, 2000);
+</script>
+
+<template>
+  <TeamHeader @add="showModal = !showModal" />
+  <div class="place-self-center flex flex-col gap-y-3">
+    <TeamMembers />
+  </div>
+  <teamFooter />
+  <Modal :show="showModal" @close="showModal = false">
+
+    <!-- Using slots to override the default content if we want -->
+    <template #header>
+      Overrided Header
+    </template>
+    <template #body>
+      <p>
+        Need to add a new member to the team?
+      </p>
+      <form>
+        <input type="text" placeholder="Add New Member">
+        <button type="submit">
+          Add
+        </button>
+      </form>
+
+    </template>
+    <template #footer>
+      Overrided Footer
+    </template>
+  </Modal>
+</template>
